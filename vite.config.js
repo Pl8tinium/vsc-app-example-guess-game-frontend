9e9432ac14ffac74f8cf9216ec9ca0764cc66699
Pl8tinium <50824212+Pl8tinium@users.noreply.github.com>
2024-09-02 21:04:44 +0000

    fix gh pages deploy

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: '/vsc-app-example-guess-game-frontend/',  
   server: {
     proxy: {
       '/api': 'http://localhost:3000',
